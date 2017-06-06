a77fcb9c4d2eb15736ca4b079e0b5fa8c3bec52b
yiqingzhu007 <yiqingz@uchicago.edu>
2017-06-06 18:05:27 -0500

    Add files via upload

--- Codes/analysis.py
+++ Codes/analysis.py
@@ -17,19 +17,18 @@ for i in range(len(data)):
 
 recom_result['user_sim'] = pd.to_numeric(recom_result['user_sim'])
 
-success_rate = len(recom_result[(recom_result['recom_success'] == True)]) 
-			   / len(recom_result)
+success_rate = len(recom_result[(recom_result['recom_success'] == True)]) / len(recom_result)
 
 high_sim_success_rate = len(recom_result[(recom_result['recom_success'] == True)
-							& (recom_result['user_sim'] >= 0.5)]) 
-						/ len(recom_result)
+							& (recom_result['user_sim'] >= 0.5)]) / len(recom_result)
 
 low_sim_success_rate = len(recom_result[(recom_result['recom_success'] == True)
-						   & (recom_result['user_sim'] < 0.5)]) 
-					   / len(recom_result)
+						   & (recom_result['user_sim'] < 0.5)]) / len(recom_result)
 
 print('The overall successful rate of the recommendation system is {}; \
 the successful rate for the significantly similar users is {}, \
 and for the insignificantly similar users is {}.'\
 .format(success_rate, high_sim_success_rate, low_sim_success_rate))
+
+recom_result.to_csv('recommendation_results.csv')
 
